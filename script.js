aa2945c6ee849836cc613eeb5d1a06180836075c
William27310 <william.27@free.fr>
2025-07-01 10:10:25 +0200

    gallery3

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-let NbExample = 5
+let NbExample = 10
 // console.log("NbExample")
 
+// for (let i = 0; i < NbExample; i++) {
+//     document.getElementById("img-js").innerHTML += 
+//             `
+//             <a class="example-image-link" href="https://picsum.photos/200/300?random=${i}"
+//                 data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+//                 <img class="example-image" src="https://picsum.photos/200/300?random=${i}" alt="" />
+//             </a>
+//             `
+// }
+
 for (let i = 0; i < NbExample; i++) {
+    let randomNb = Math.floor(Math.random() * 200)
     document.getElementById("img-js").innerHTML += 
             `
-            <a class="example-image-link" href="https://picsum.photos/200/300?random=${i}"
+            <div class="col-3 mb-5 m-auto">
+            <a class="example-image-link" href="https://picsum.photos/id/${randomNb}/200/300"
                 data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
-                <img class="example-image" src="https://picsum.photos/200/300?random=${i}" alt="" />
+                <img class="example-image" src="https://picsum.photos/id/${randomNb}/200/300" alt="" />
             </a>
+            </div>
             `
 }
-
